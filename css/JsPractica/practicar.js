f7476168224c5b1132d8b565771f730b6046dfe3
David Hernandez <davyd2h@gmail.com>
2023-11-19 08:21:36 -0500

    JS de práctica, estructura HERO HTML

---
+++ css/JsPractica/practicar.js
@@ -0,0 +1,17 @@
+/*
+Ejercicio: Suma de números pares en un rango
+
+Crea una función llamada sumaNumerosParesque tome dos argumentos: inicioy fin, que representan el rango de números a considerar.
+
+Dentro de la función, utilice un bucle forpara iterar a travésiniciohasta fin.
+
+En cada iteración, verifica si el número actual es pa%(módulo) para verificar si el número es divisible por 2 sin dejar un r
+
+Si el número es par, agregue una llamada variable suma.
+
+Al finalizar el bucle, la variable `susumacontendrá la suma de todos los números pares dentro del rango.
+
+La función debe devolver el valor de suma.
+
+Llama a la función sumaNumerosParescon un rango específico, por ejemplo, inicio = 1y fin = 10, y debe
+*/
