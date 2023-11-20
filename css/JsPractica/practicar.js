fa987c15ef0d6c5164c6cab65127f1ad6ea79972
David Hernandez <davyd2h@gmail.com>
2023-11-20 10:39:49 -0500

    se crea un main y la descripción, igual que las etiquetas de iconos

--- css/JsPractica/practicar.js
+++ css/JsPractica/practicar.js
@@ -3,7 +3,7 @@ Ejercicio: Suma de números pares en un rango
 
 Crea una función llamada sumaNumerosParesque tome dos argumentos: inicioy fin, que representan el rango de números a considerar.
 
-Dentro de la función, utilice un bucle forpara iterar a travésiniciohasta fin.
+Dentro de la función, utilice un bucle for para iterar a través inicio hasta fin.
 
 En cada iteración, verifica si el número actual es pa%(módulo) para verificar si el número es divisible por 2 sin dejar un r
 
@@ -15,3 +15,16 @@ La función debe devolver el valor de suma.
 
 Llama a la función sumaNumerosParescon un rango específico, por ejemplo, inicio = 1y fin = 10, y debe
 */
+
+function sumaNumerosPares(inicio, fin) {
+    let suma = 0;    
+        for(let numero = inicio; numero <= fin; numero++) {
+            if (numero % 2 === 0){
+                suma += numero;
+        }
+        return suma;
+    }
+}
+
+const resultado = sumaNumerosPares(1,10);
+console.log(`La suma de números pares en el rango es: ${resultado}`);
